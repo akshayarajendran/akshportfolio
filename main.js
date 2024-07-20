ffe0216f1b22c9adb02810797c503aef7ce67a64
Akshaya <akshaya19052004@gmail.com>
2024-07-21 00:15:11 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// Menu icon toggle
+let menuIcon = document.getElementById('menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+// Scroll section active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.header nav a[href*="' + id + '"]').classList.add('active');
+            });
+        }
+    });
+    let header=document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY>100);
+
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
+};
+
+
+
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p', { origin: 'right' });
+
+// Typed.js animations
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Engineer', 'Frontend Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop:true, 
+});
